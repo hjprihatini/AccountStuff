626710aee24558314972b594ef71cfb2c54d9898
REDSEC <68374733+hjprihatini@users.noreply.github.com>
2022-04-17 20:30:48 +0700

    Add files via upload

---
+++ AccountStuffVIP.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+clear
+figlet AccountStuff
+echo "Version 1.2.0"
+echo
+echo "#############################"
+echo "Name : RedSec"
+echo "Program : Bash"
+echo "Youtube : https://youtube.com/RED SEC"
+echo "Team : Cysec || MedanSec || RSTeam"
+echo "Github : https://github.com/hjprihatini"
+echo "Pesan : Akun yang di sediakan dijamin 100% bisa login tapi biasa nya limit"
+echo "Sebelum Marah Marah Ngerti Dulu Apa itu LIMIT"
+echo "#############################"
+echo
+echo "[+] Ketik 1 untuk mulai"
+echo
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] [Run]~# \e[0m\en' option
+echo
+echo "Proses......"
+sleep 3
+echo "Sukses Run Account"
+echo
+sleep 1
+echo "LIVE -> kormi0617@gmail.com:Demeter251"
+sleep 1
+echo "LIVE -> alialp756@gmail.com:Aligs121"
+sleep 1
+echo "LIVE -> aryanda.killer@gmail.com:Aryanda19951"
+sleep 1
+echo "LIVE -> ideia.aurelie@gmail.com:Az2agkdj1231"
+sleep 1
+echo "LIVE -> proalexgames@gmail.com:Vadimka11"
+sleep 1
+echo "LIVE -> candyz196hacker@gmail.com:Mayor1"
+sleep 1
+echo "LIVE -> tuddin8@yahoo.com:Taj11"
+sleep 1
+echo "No Live -> papaicorudo@ya.ru:874521"
+sleep 1
+echo "No Live -> aanzid@aol.com:Wildcats16"
+sleep 1
+echo "No Live -> pavmakpes@yandex.com:pavluha09"
+sleep 1
+echo "LIVE -> candyz196hacker@gmail.com:Mayor1"
+sleep 1
+echo "No Live -> mickaelh@gmail.com:gemini1337"
+sleep 1
+echo "No Live -> micky_pz@hotmail.com:pulsar10"
+sleep 1
+echo "LIVE -> tuddin8@yahoo.com:Taj11"
+sleep 1
+echo "LIVE -> depandienda@gmail.com:Degamdesil2211"
+sleep 1
+echo "No Live -> paul.balakin@yandex.ru:Balakin2003"
+sleep 1
+echo "No Live -> pilipenko.roman2016@ya.ru:0123456789"
+sleep 1
+echo "No Live -> knaranjof@hotmail.com:florencia1"
+sleep 1
+echo "LIVE -> angohjer23@gmail.com:Syahrul2311"
+sleep 1
+echo "LIVE -> SpiriT.exe@wp.pl:Kevin40011"
+sleep 1
+echo "LIVE -> elpguitar@gmail.com:Iceiceice911"
+sleep 2
+echo 
+echo "TERIMA KASIH :) SUBS CHANNEL REDSEC"
